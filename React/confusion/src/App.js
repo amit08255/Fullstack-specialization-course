b7cee15f0fd0aeea8cc1d8c8614de3e872fd8832
amit <amit@schooglink.com>
2020-06-14 19:31:59 +0530

    Configuring React.

---
+++ React/confusion/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css';
+import { Navbar, NavbarBrand } from 'reactstrap';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <Navbar dark color="primary">
+          <div className="container">
+            <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
+          </div>
+        </Navbar>
+      </div>
+    );
+  }
+}
+
+export default App;
